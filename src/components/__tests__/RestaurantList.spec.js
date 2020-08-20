564177bb85dab42e8710668be59058a225a6ded5
Jeff Chong <zifzhong@yeah.net>
2020-08-20 19:54:15 +0800

    Refact RestaurantsList test

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -3,32 +3,31 @@ import { render, cleanup } from '@testing-library/react';
 import { RestaurantList } from '../RestaurantList';
 
 describe('<RestaurantList />', () => {
-  afterEach(cleanup);
+  const restaurants = [
+    { id: 1, name: 'Sushi Place' },
+    { id: 2, name: 'Pizza Place' },
+  ];
+  let context;
+  let loadRestaurants;
 
-  it('渲染时调用 loadRestaurants 函数', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
-
-    render(
+  beforeEach(() => {
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
+    context = render(
       <RestaurantList
         loadRestaurants={loadRestaurants}
         restaurants={restaurants}
       />,
     );
+  });
 
+  afterEach(cleanup);
+
+  it('渲染时调用 loadRestaurants 函数', () => {
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('显示餐馆列表', () => {
-    const noop = () => { };
-    const restaurants = [
-      { id: 1, name: 'Sushi Place' },
-      { id: 2, name: 'Pizza Place' },
-    ];
-
-    const { queryByText } = render(
-      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
-    );
+    const { queryByText } = context;
 
     // queryByText 为查询到值时返回 null
     expect(queryByText('Sushi Place')).not.toBeNull();
