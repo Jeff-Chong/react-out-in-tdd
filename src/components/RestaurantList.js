444ecf1b6e4560c1c5d1698c1f3e7698a6046a53
Jeff Chong <zifzhong@yeah.net>
2020-08-20 19:50:21 +0800

    Displays the restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
 
-export function RestaurantList({ loadRestaurants }) {
+export function RestaurantList({ loadRestaurants, restaurants }) {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
-  return <div>RestaurantList</div>;
+  return (
+    <ul>
+      {restaurants.map(restaurant => (
+        <li key={restaurant.name}>{restaurant.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default RestaurantList;
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { RestaurantList } from '../RestaurantList';
 
 describe('<RestaurantList />', () => {
-  it('当第一次渲染时读取数据', () => {
+  afterEach(cleanup);
+
+  it('渲染时调用 loadRestaurants 函数', () => {
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    const restaurants = [];
+
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('显示餐馆列表', () => {
+    const noop = () => { };
+    const restaurants = [
+      { id: 1, name: 'Sushi Place' },
+      { id: 2, name: 'Pizza Place' },
+    ];
+
+    const { queryByText } = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    // queryByText 为查询到值时返回 null
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
